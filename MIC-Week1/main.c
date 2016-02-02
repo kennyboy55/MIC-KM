8706fce0a7b446c5dabf7d51b1271571e99bbda3
Kenneth van Ewijk <kennyboy@live.nl>
2016-02-02 11:49:09 +0100

    project init

---
+++ MIC-Week1/main.c
@@ -0,0 +1,18 @@
+/*
+ * MIC-Week1.c
+ *
+ * Created: 2-2-2016 11:48:44
+ * Author : kenny
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
