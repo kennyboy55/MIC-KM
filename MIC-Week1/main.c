e3c373c92067640522012514b15b881813fcdd45
Kenneth van Ewijk <kennyboy@live.nl>
2016-02-02 14:52:23 +0100

    Opgave 1

--- MIC-Week1/main.c
+++ MIC-Week1/main.c
@@ -5,14 +5,44 @@
  * Author : kenny
  */ 
 
-#include <avr/io.h>
+#define F_CPU 8000000UL
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+/******************************************************************/
+void wait( int ms )
+/* 
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+Version :    	DMK, Initial code
+*******************************************************************/
+{
+	for (int i=0; i<ms; i++)
+	{
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRD = 0b11111111;			// All pins PORTD are set to output
+
+	PORTD = 0b00000001;
+
+	while (1)
+	{
+		PORTD = 0b10000000; 
+		wait( 500 );
+		PORTD = 0b01000000;
+		wait ( 500 );
+		
+	}
+
+	return 1;
 }
 
